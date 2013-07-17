8eb88e2c4a0619ea9fa66cfa4dbd9896d2fccda6
Ryan Elmore <Ryan.Elmore@nrel.gov>
2013-07-17 13:36:11 -0600

    initial

---
+++ test.R
@@ -0,0 +1,12 @@
+## Ryan Elmore
+## Testing RStudio/Git Integration
+## 17 July 2013
+## Jeebus
+
+e <- rnorm(100, s = 10)
+x <- 5 + 10*runif(100, 0, 10)
+hist(x)
+y <- x + e
+plot(x, y)
+## another
+## huh?
